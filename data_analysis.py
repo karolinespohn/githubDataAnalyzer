175867f744a8c734310acc69f189da39a284e0f9
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2024-05-04 17:02:16 +0200

    added function get_dev_with_most_changes_per_commit() which iterates through set mapping dev to average changes per commit and returns max

--- data_analysis.py
+++ data_analysis.py
@@ -16,7 +16,7 @@ def get_frequent_collaborators(file_to_committers_map):
         sorted_committers = sorted(committers.items(), key=lambda x: x[0])
 
         for i in range(len(sorted_committers)):
-            for j in range (i + 1, len(sorted_committers)):
+            for j in range(i + 1, len(sorted_committers)):
                 committer1, committer1_count = sorted_committers[i]
                 committer2, committer2_count = sorted_committers[j]
 
@@ -34,3 +34,23 @@ def get_frequent_collaborators(file_to_committers_map):
                     max_shared_commits = shared_commits
 
     return max_pairs
+
+
+def get_dev_with_most_changes_per_commit(dev_to_changes_map):
+    max_changes_per_commit = 0
+    devs_with_most_changes = []
+
+    for dev in dev_to_changes_map:
+        data = dev_to_changes_map[dev]
+        print(data)
+        changes = data[2]
+        if int(changes) == max_changes_per_commit:
+            devs_with_most_changes.append(dev)
+        if int(changes) > max_changes_per_commit:
+            max_changes_per_commit = changes
+            devs_with_most_changes = [dev]
+
+    if max_changes_per_commit == 0:
+        return None
+
+    return devs_with_most_changes

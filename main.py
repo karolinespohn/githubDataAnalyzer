8bca4012d718f498e0242bc97048b897e69a6cac
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2024-05-04 18:54:25 +0200

    added option parsing function

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from dotenv import load_dotenv
 import argparse
 import execute_analysis
 
+
 def main(args):
     load_dotenv()
     token = os.getenv("GITHUB_API_TOKEN")
+    owner, repo, fun = option_parsing(args)
 
+    match fun:
+        case "1": execute_analysis.find_frequent_collaborators_by_commits(owner, repo, token)
+        case "2": execute_analysis.find_frequent_collaborators_by_changes(owner, repo, token)
+        case "3": execute_analysis.find_devs_with_largest_commits(owner, repo, token)
+        case "4": execute_analysis.find_devs_with_smallest_commits(owner, repo, token)
+        case "5": execute_analysis.find_devs_with_longest_commit_messages(owner, repo, token)
+        case _: "Please specify, what kind of analysis you would like to perform"
+
+
+def option_parsing(args):
     repository = args.url
     if "github.com/" not in repository:
         print("URL of repository is invalid")
@@ -20,17 +32,10 @@ def main(args):
     try:
         owner = url_components[url_components.index("github.com") + 1]
         repo = url_components[url_components.index("github.com") + 2]
+
+        return owner, repo, fun
     except IndexError:
         print("URL of repository is invalid")
-        return
-
-    match fun:
-        case "1": execute_analysis.find_frequent_collaborators_by_commits(owner, repo, token)
-        case "2": execute_analysis.find_frequent_collaborators_by_changes(owner, repo, token)
-        case "3": execute_analysis.find_devs_with_largest_commits(owner, repo, token)
-        case "4": execute_analysis.find_devs_with_smallest_commits(owner, repo, token)
-        case "5": execute_analysis.find_devs_with_longest_commit_messages(owner, repo, token)
-        case _: "Please specify, what kind of analysis you would like to perform"
 
 
 if __name__ == '__main__':
@@ -38,5 +43,4 @@ if __name__ == '__main__':
     parser.add_argument("--url", required=True, help="URL of the GitHub repository")
     parser.add_argument("--ana", required=True, help="Analysis to be performed")
 
-    args = parser.parse_args()
-    main(args)
+    main(parser.parse_args())

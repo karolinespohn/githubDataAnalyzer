b6aaf82670e0a5650d87f520997d476fd703ba70
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2024-05-04 16:13:02 +0200

    added function that maps file to each person contributing to that file and their respective amount of changes to file

--- data_structuring.py
+++ data_structuring.py
@@ -6,3 +6,10 @@ def map_file_to_commits_per_person(commits):
     for committer, file in commits:
         file_contributors_map[file][committer] += 1
     return file_contributors_map
+
+
+def map_file_to_changes_per_person(commits):
+    file_contributors_map = defaultdict(lambda: defaultdict(int))
+    for committer, file, changes in commits:
+        file_contributors_map[file][committer] += changes
+    return file_contributors_map

a67a718a11e6561a7739c17b304378306285202d
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2024-05-04 16:46:53 +0200

    added map_dev_to_changes which takes a list of tuples of the form (name, additions, deletions) and returns a map mapping each dev to the sum of all changes, sum of commits and average changes per commit

--- data_structuring.py
+++ data_structuring.py
@@ -2,14 +2,29 @@ from collections import defaultdict
 
 
 def map_file_to_commits_per_person(commits):
-    file_contributors_map = defaultdict(lambda: defaultdict(int))
+    file_to_contributors_map = defaultdict(lambda: defaultdict(int))
     for committer, file in commits:
-        file_contributors_map[file][committer] += 1
-    return file_contributors_map
+        file_to_contributors_map[file][committer] += 1
+    return file_to_contributors_map
 
 
 def map_file_to_changes_per_person(commits):
-    file_contributors_map = defaultdict(lambda: defaultdict(int))
+    file_to_contributors_map = defaultdict(lambda: defaultdict(int))
     for committer, file, changes in commits:
-        file_contributors_map[file][committer] += changes
-    return file_contributors_map
+        file_to_contributors_map[file][committer] += changes
+    return file_to_contributors_map
+
+
+def map_dev_to_changes(commits):
+    dev_to_changes_map = defaultdict(lambda: [0, 0, 0])  # dev -> changes, commits, average
+    for commit in commits: # (name, additions, deletions)
+        changes = commit[1] + commit[2]
+
+        entry = dev_to_changes_map[commit[0]]
+        entry[0] = entry[0] + changes
+        entry[1] = entry[1] + 1
+        entry[2] = entry[0] / entry[1]
+
+    return dev_to_changes_map
+
+
